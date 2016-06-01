393718d37fbff2a21b3e13e862be1704874b48db
John Chadwick <johnwchadwick@gmail.com>
2016-06-01 11:31:09 -0400

    Update to the latest Atlassian JIRA supported jQuery version :)

--- jira/click-to-issue.user.js
+++ jira/click-to-issue.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         JIRA Click to Issue
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Force issues to open full-page.
 // @author       John
 // @match        https://benzinga.atlassian.net/*
 // @grant        none
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js
 // ==/UserScript==
 
 (function() {
